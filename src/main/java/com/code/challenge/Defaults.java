8bfd09950a068ce98190811e65fed9abb90e7a71
prashant-byte <68318507+prashant-byte@users.noreply.github.com>
2020-07-22 01:32:19 -0400

    Create Defaults.java

---
+++ src/main/java/com/code/challenge/Defaults.java
@@ -0,0 +1,13 @@
+package com.code.challenge;
+
+/**
+ *
+ * Code challenge defaults class.
+ *
+ * @author  Prashant Kumar
+ * @version 1.0
+ * @since   2020-07-15
+ */
+public class Defaults {
+    public static String cityFile = "city.txt";
+}
